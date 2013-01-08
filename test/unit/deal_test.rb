9333d72524570de1c15c83c227463758ca7ec288
Tim Prentis <tprentis@gmail.com>
2013-01-08 15:23:13 -0500

    TPP Story03 changes

--- test/unit/deal_test.rb
+++ test/unit/deal_test.rb
@@ -5,11 +5,12 @@ class DealTest < ActiveSupport::TestCase
     assert FactoryGirl.build(:deal).valid?
   end
 
-  # I think this is a bad test and it fails sometimes
-  test "over should honor current time" do
-  	deal = FactoryGirl.create(:deal, :end_at => Time.zone.now + 0.01)
-  	assert !deal.over?, "Deal should not be over"
-  	sleep 1
-  	assert deal.over?, "Deal should be over"
-  end
+# TPP - changed 0.01 to 1.second
+  test "1 over should be less than current time" do
+    deal = FactoryGirl.create(:deal, :end_at => Time.zone.now + 1.second)
+    assert !deal.over?, "Deal should not be over"
+    sleep 1
+    assert deal.over?, "Deal should be over"
+  end  
+
 end
